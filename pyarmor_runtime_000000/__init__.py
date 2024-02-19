2fea0767581ad4133d55e4cfbebde576211e088d
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2024-02-19 01:00:45 -0800

    Better error handling

--- pyarmor_runtime_000000/__init__.py
+++ pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.4.7 (trial), 000000, 2024-02-19T00:33:32.141335
+# Pyarmor 8.4.7 (trial), 000000, 2024-02-19T00:53:00.483500
 from .pyarmor_runtime import __pyarmor__
--- setup.bat
+++ setup.bat
@@ -1,10 +1,10 @@
-@echo off 
+@echo off
 Title Downloading Modules...
 python --version 3>NUL
 if errorlevel 1 goto errorNoPython
 pip -v>NUL
 if errorlevel 1 goto errorNoPip
 python -m pip install -r assets/requirements.txt
-cls
+echo ------------------------------------------------------------------------
 echo Done! Please use config.json to edit important settings
 pause
