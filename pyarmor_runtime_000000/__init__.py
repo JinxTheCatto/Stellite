a6dbf7f0db496a41ac95755fababd19f5792c161
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2024-02-18 19:51:51 -0800

    Better error handling and color overrides

--- pyarmor_runtime_000000/__init__.py
+++ pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.4.7 (trial), 000000, 2024-02-18T00:40:22.237096
+# Pyarmor 8.4.7 (trial), 000000, 2024-02-18T19:20:08.041425
 from .pyarmor_runtime import __pyarmor__
---
+++ update.bat
@@ -0,0 +1,5 @@
+@echo off
+git init
+git pull https://github.com/JinxTheCatto/Stellite.git
+echo Done updating!
+pause
