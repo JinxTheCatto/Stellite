9983c2d4be13427649afab406152ad158cbc49af
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2024-02-21 16:07:45 -0800

    Less instructions

--- setup.bat
+++ setup.bat
@@ -1,9 +1,5 @@
 @echo off
 Title Downloading Modules...
-python --version 3>NUL
-if errorlevel 1 goto errorNoPython
-pip -v>NUL
-if errorlevel 1 goto errorNoPip
 python -m pip install -r assets/requirements.txt
 echo ------------------------------------------------------------------------
 echo Done! Please use config.json to edit important settings
