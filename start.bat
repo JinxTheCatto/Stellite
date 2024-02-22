9cc83f0190cbc12351f44209c622135d16d46135
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2024-02-22 12:35:53 -0800

    Change syntax

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-py bot.py
+python bot.py
 pause
--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
-echo You must have git installed and on PATH to use the update program. Continue if this is fine
+echo You must have git installed and on PATH to use the update script. Continue if this is fine
 pause
 git init
 git pull https://github.com/JinxTheCatto/Stellite.git
