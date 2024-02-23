20ccb6861b6b5638ef3101ba1bfa9851fcd57d74
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2024-02-23 12:16:46 -0800

    Hotfix webcheck

--- pyarmor_runtime_000000/__init__.py
+++ pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.4.7 (trial), 000000, 2024-02-22T12:06:01.423126
+# Pyarmor 8.4.7 (trial), 000000, 2024-02-23T12:16:09.501732
 from .pyarmor_runtime import __pyarmor__
--- start.bat
+++ start.bat
@@ -1,3 +1,2 @@
-@echo off
 python bot.py
 pause
