269b94f752cc2fa6c2db8a1f53d2eb3400c0c553
jinx <59938808+JinxTheCatto@users.noreply.github.com>
2025-01-16 19:16:16 -0800

    Large refactoring, more robust. New image format

--- bot.py
+++ bot.py
@@ -34,28 +34,21 @@ except Exception as e:
 monitor_width = ctypes.windll.user32.GetSystemMetrics(0)
 monitor_height = ctypes.windll.user32.GetSystemMetrics(1)
 
-# Ensure console window is on top
-hwnd = None  # Global variable to store the console window handle
-
-def keep_console_on_top():
+if config.get("console_window_ontop") == "true":
     global hwnd
-    hwndlist = []
+    hwnd_list = []
     def findit(hwnd, ctx):
         if win32gui.GetWindowText(hwnd).find("Stellite Autoplayer Console") != -1:
-            hwndlist.append(hwnd)
+            hwnd_list.append(hwnd)
     win32gui.EnumWindows(findit, None)
-    if len(hwndlist) == 1:
-        hwnd = hwndlist[0]
+    if len(hwnd_list) == 1:
+        hwnd = hwnd_list[0]
         try:
             win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
         except Exception as e:
             print(Back.RED + Fore.WHITE + f"ERROR: {str(e)} while setting window position." + Back.RESET + Fore.RESET)
     else:
         print(Back.RED + Fore.WHITE + "ERROR: Console window handle not found or multiple windows detected." + Back.RESET + Fore.RESET)
-
-# Call once if necessary
-if config.get("console_window_ontop") == "true":
-    keep_console_on_top()
 
 # START
 print(f'''
@@ -80,30 +73,17 @@ else:
 assert number_of_lanes in [4, 5], Back.RED + Fore.WHITE + "Number of lanes must be 4 or 5" + Back.RESET + Fore.RESET
 
 print(Fore.GREEN + f"WILL ONLY AUTOPLAY WHILE CAPS LOCK IS ON! CURRENT CAPSLOCK STATUS: " + ("On" if win32api.GetKeyState(0x14) else "Off"))
-print(Fore.YELLOW + f"Capture region color mode: {config['color_mode']}" + Fore.RESET)
-print(Fore.YELLOW + f"Viewbox color mode: {config['color_mode']}")
-print(f"Resolution: {monitor_width}x{monitor_height}" + Fore.RESET)
+print(Fore.YELLOW + f"Resolution: {monitor_width}x{monitor_height}" + Fore.RESET)
 print(f"Program started, press {str.upper(config['exit_key'])} to exit")
 
-imagefound = False
+image_found = False
 for filename in os.listdir("assets"):
-    if filename.startswith(f"tile{monitor_height}"):
-        imagefound = True
+    if filename == str(monitor_height):
+        image_found = True
         break
-if not imagefound:
-    print(Back.RED + Fore.WHITE + "No match images in the assets folder found with the current monitor height! Please make sure you have an image made, or contact me in the discord to help me add support!" + Back.RESET + Fore.RESET)
+if not image_found:
+    print(Back.RED + Fore.WHITE + "No match images in the assets folder found with the current monitor height! Please make sure you have an image made." + Back.RESET + Fore.RESET)
     exit()
-
-tile_filenames = [
-    f'assets/tile{monitor_height}{config["tile_filename_suffix"]}.png',
-]
-#if config["detect_hold_tiles"] == "true":
-    #tile_filenames.append(f'assets/tile{monitor_height}hold{config["tile_filename_suffix"]}.png')
-if config["color_mode"] != "gray" and config["use_white_tile"] == "true":
-    tile_filenames.append(f'assets/tile{monitor_height}white{config["tile_filename_suffix"]}.png')
-if config["detect_diamond_tiles"] == "true":
-    tile_filenames.append(f'assets/tile{monitor_height}diamond{config["tile_filename_suffix"]}.png') # the way i detect if the tile being checked is the diamond tile is so retarded
-tile_images = [cv2.imread(tile_filename, (cv2.IMREAD_GRAYSCALE if config["color_mode"] == "gray" else cv2.IMREAD_UNCHANGED)) for tile_filename in tile_filenames]
 
 # 1080p values:
 region_width = 555 if number_of_lanes == 4 else 696 # width of the capture region
@@ -123,18 +103,9 @@ width = region_fromleft + region_width
 height = region_fromtop + region_height
 section_size = region_width // number_of_lanes
 
-#if config["viewbox_bgra_color_override"] == "true":
-    #viewboxcamera = bettercam.create(output_color="BGRA", max buffer_len=512)
-    #viewboxcamera.start(region=(region_fromleft, region_fromtop, width, height), target_fps=60)
-
-maincamera = bettercam.create(output_color = ("GRAY" if config["color_mode"] == "gray" else "BGRA"), max_buffer_len=512)
-maincamera.start(region = (region_fromleft, region_fromtop, width, height), target_fps=config["capture_fps"])
-if config["show_debug_visuals"] == "true":
-    cv2.namedWindow('Stellite Autoplayer View')
-    cv2.resizeWindow('Stellite Autoplayer View', region_width, region_height)
-    cv2.moveWindow('Stellite Autoplayer View', (monitor_width - region_width) - 10, 0)
-    cv2.setWindowProperty('Stellite Autoplayer View', cv2.WINDOW_FREERATIO, 0)
-    cv2.setWindowProperty('Stellite Autoplayer View', cv2.WND_PROP_TOPMOST, 1)
+main_camera = bettercam.create(output_color = "GRAY", max_buffer_len=512)
+main_camera.start(region = (region_fromleft, region_fromtop, width, height), target_fps=config["capture_fps"])
+boxed_screenshot = main_camera.get_latest_frame()
 
 lane_cooldowns = {
     str(config["key_1"]): 0.0,
@@ -144,13 +115,17 @@ lane_cooldowns = {
     str(config["key_5"]): 0.0
 }
 
+def truncate(n, decimals = 0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
 def cooldown(key):
     lane_cooldowns[key] = config["max_lane_cooldown"]
     time.sleep(config["max_lane_cooldown"])
     lane_cooldowns[key] = 0.0
 
 def press(key, hold = False):
-    print(Fore.GREEN + f"{time.time()}: Pressing {key}" + Fore.RESET)
+    print(Fore.GREEN + f"{time.time()}: Pressing {str.capitalize(key)}" + Fore.RESET)
     # if keyboard.is_pressed(key): 
     #    keyboard.release(key)
     keyboard.press(key)
@@ -164,62 +139,100 @@ def press_tile(key, hold = False):
     else:
         print(Fore.RED + f"Skipping lane {key} due to cooldown" + Fore.RESET)
 
+tile = cv2.imread(f'assets/{monitor_height}/tile{config["tile_filename_suffix"]}.png', cv2.IMREAD_GRAYSCALE)
+tile_width = tile.shape[1]
+tile_height = tile.shape[0]
+tile = tile.astype(np.uint8)
+if config["use_white_tile"] == "true":
+    white_tile = cv2.imread(f'assets/{monitor_height}/white{config["tile_filename_suffix"]}.png', cv2.IMREAD_GRAYSCALE)
+    white_tile_width = white_tile.shape[1]
+    white_tile_height = white_tile.shape[0]
+    white_tile = white_tile.astype(np.uint8)
+if config["use_battlestage_tile"] == "true":
+    battlestage_tile = cv2.imread(f'assets/{monitor_height}/battlestage{config["tile_filename_suffix"]}.png', cv2.IMREAD_GRAYSCALE)
+    battlestage_tile_width = battlestage_tile.shape[1]
+    battlestage_tile_height = battlestage_tile.shape[0]
+    battlestage_tile = battlestage_tile.astype(np.uint8)
+if config["use_diamond_tile"] == "true":
+    diamond_tile = cv2.imread(f'assets/{monitor_height}/diamond{config["tile_filename_suffix"]}.png', cv2.IMREAD_GRAYSCALE)
+    diamond_tile_width = diamond_tile.shape[1]
+    diamond_tile_height = diamond_tile.shape[0]
+    diamond_tile = diamond_tile.astype(np.uint8)
+
+def press_tiles(rectangles, results):
+    for (x, y, w, h) in rectangles:
+        tile_position = (x + (w // 2), y + (h // 2))
+        #print(f"Found tile at {tile_position} confidence: 
+        tile_lane = config[f"key_{tile_position[0] // section_size + 1}"]
+        if tile_position[1] >= min_tile_pixels_top_offset:
+            press_tile(tile_lane)
+
+        if config["debug_positions"]:
+            print(f"Found tile in lane {str.capitalize(tile_lane)} with confidence {truncate(results[y][x], 2)}")
+
+def tile_logic(screenshot_np):
+    results = cv2.matchTemplate(screenshot_np, tile, cv2.TM_CCOEFF_NORMED)
+    locations = np.where(results >= config["min_confidence"])
+
+    rectangles = []
+    for positions in zip(*locations[::-1]):
+        rectangles.append((int(positions[0]), int(positions[1]), int(tile_width), int(tile_height)))
+        rectangles.append((int(positions[0]), int(positions[1]), int(tile_width), int(tile_height)))
+    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+
+    threading.Thread(target=press_tiles, args=(rectangles, results)).start()
+
+def white_tile_logic(screenshot_np):
+    results = cv2.matchTemplate(screenshot_np, white_tile, cv2.TM_CCOEFF_NORMED)
+    locations = np.where(results >= config["white_tile_min_confidence"])
+
+    rectangles = []
+    for positions in zip(*locations[::-1]):
+        rectangles.append((int(positions[0]), int(positions[1]), int(white_tile_width), int(white_tile_height)))
+        rectangles.append((int(positions[0]), int(positions[1]), int(white_tile_width), int(white_tile_height)))
+    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+
+    threading.Thread(target=press_tiles, args=(rectangles, results)).start()
+
+def battlestage_tile_logic(screenshot_np):
+    results = cv2.matchTemplate(screenshot_np, battlestage_tile, cv2.TM_CCOEFF_NORMED)
+    locations = np.where(results >= config["battlestage_tile_min_confidence"])
+
+    rectangles = []
+    for positions in zip(*locations[::-1]):
+        rectangles.append((int(positions[0]), int(positions[1]), int(battlestage_tile_width), int(battlestage_tile_height)))
+        rectangles.append((int(positions[0]), int(positions[1]), int(battlestage_tile_width), int(battlestage_tile_height)))
+    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+
+    threading.Thread(target=press_tiles, args=(rectangles, results)).start()
+
+def diamond_tile_logic(screenshot_np):
+    results = cv2.matchTemplate(screenshot_np, diamond_tile, cv2.TM_CCOEFF_NORMED)
+    locations = np.where(results >= config["diamond_tile_min_confidence"])
+
+    rectangles = []
+    for positions in zip(*locations[::-1]):
+        rectangles.append((int(positions[0]), int(positions[1]), int(diamond_tile_width), int(diamond_tile_height)))
+        rectangles.append((int(positions[0]), int(positions[1]), int(diamond_tile_width), int(diamond_tile_height)))
+    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+
+    threading.Thread(target=press_tiles, args=(rectangles, results)).start()
+
 while not keyboard.is_pressed(config['exit_key']):
-    screenshot_np = maincamera.get_latest_frame()
+    screenshot_np = main_camera.get_latest_frame()
     capslock_status = win32api.GetKeyState(0x14)
     if capslock_status and screenshot_np is not None:
-        for tile_image in tile_images:  # this could use multithreading
-            tile_image = tile_image.astype(screenshot_np.dtype)
-            tilewidth = tile_image.shape[1]
-            tileheight = tile_image.shape[0]
-
-            result = cv2.matchTemplate(screenshot_np, tile_image, cv2.TM_CCOEFF_NORMED)
-            # don't fix it if it ain't broke
-            minimumdetectconfidence = ((config["diamond_tiles_min_confidence"] if tile_image.shape == tile_images[len(tile_images) - 1].shape else config["min_confidence"]) if config["detect_diamond_tiles"] == "true" else config["min_confidence"])
-            locations = np.where(result >= minimumdetectconfidence)
-
-            rectangles = []
-            for positions in zip(*locations[::-1]):
-                rectangles.append((int(positions[0]), int(positions[1]), int(tilewidth), int(tileheight)))
-                rectangles.append((int(positions[0]), int(positions[1]), int(tilewidth), int(tileheight)))
-            rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
-
-            for (x, y, w, h) in rectangles:
-                tile_position = (x + (tilewidth // 2), y + (tileheight // 2))
-                print(f"Detected tile at position {tile_position} with confidence {result[positions[1], positions[0]]}")
-                if tile_position[1] >= min_tile_pixels_top_offset:
-                    if tile_position[0] <= section_size:
-                        press_tile(config["key_1"])
-                    elif tile_position[0] <= section_size * 2:
-                        press_tile(config["key_2"])
-                    elif tile_position[0] <= section_size * 3:
-                        press_tile(config["key_3"])
-                    elif tile_position[0] <= section_size * 4:
-                        press_tile(config["key_4"])
-                    elif tile_position[0] <= section_size * 5:
-                        press_tile(config["key_5"])
-
-                if config["show_debug_visuals"] == "true":
-                    if config["color_mode"] == "gray" and config["viewbox_bgra_color_override"] == "false":
-                        cv2.rectangle(screenshot_np, (x, y), (x + w, y + h), (255, 255, 255), 2)
-                    else:
-                        cv2.rectangle(screenshot_np, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-    if config["show_debug_visuals"] == "true":
-        capslock_text = "CAPS LOCK: ON" if capslock_status else "CAPS LOCK: OFF"
-        if config["color_mode"] == "gray" and config["viewbox_bgra_color_override"] == "false":
-            cv2.putText(screenshot_np, capslock_text, (0, 15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.imshow('Stellite Autoplayer View', screenshot_np)
-        else:
-            cv2.putText(screenshot_np, capslock_text, (0, 15), cv2.FONT_HERSHEY_PLAIN, 1, ((0, 255, 0) if capslock_status else (0, 0, 255)), 1, cv2.LINE_AA)
-            cv2.imshow('Stellite Autoplayer View', viewboxcamera.grab())
-        cv2.waitKey(1)
+        threading.Thread(target=tile_logic, args=(screenshot_np,)).start()
+        if config["use_white_tile"] == "true":
+            threading.Thread(target=white_tile_logic, args=(screenshot_np,)).start()
+        if config["use_battlestage_tile"] == "true":
+            threading.Thread(target=battlestage_tile_logic, args=(screenshot_np,)).start()
+        if config["use_diamond_tile"] == "true":
+            threading.Thread(target=diamond_tile_logic, args=(screenshot_np,)).start()
 
 # STOP
-cv2.destroyAllWindows()
-maincamera.stop()
+main_camera.stop()
 if config["console_window_ontop"] == "true":
     win32gui.SetWindowPos(hwnd, win32con.HWND_NOTOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
 
-# S
 print(Fore.WHITE + Back.RED + "Stop key pressed. Exiting Stellite..." + Fore.RESET + Back.RESET)
